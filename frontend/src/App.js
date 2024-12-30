7e07c73e8cda0ed24a5a8498731dfb510f811181
Harry-Joseph9387 <harryjoseph9387@gmail.com>
2024-12-30 15:49:28 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,31 @@ import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import {useState} from 'react'
+import logo from './pics/x.jpg'
+
 import './index.css'
 const App = () => {
   const [eventId,setEventid]=useState('')
+  const [loggedIn,setLoggedIn]=useState(true)
+  const [usr,setUsr]=useState({username:'xxxx',dp:logo,liked:[],registered:[]})
+  const [event,setEvent]=[
+    {
+      organiser:"Organizer",about:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, similique, maiores eligendi voluptatem libero obcaecati in nemo dignissimos autem quia, animi incidunt molestiae nulla aliquam excepturi aspernatur ad nobis nihil. Dolorem obcaecati velit voluptatem suscipit veritatis rerum fuga? Deleniti iusto similique hic cum distinctio magnam quo. Mollitia, eos. Nam sunt explicabo molestiae voluptate sapiente ad impedit sed ipsam eius architecto?",
+      title:"EventNAme",location:"asf,sd",time:"12pm",
+      comments:[
+        {username:"user1",dp:logo,comments:"safsakjnksjfs"},
+        {username:"user2",dp:logo,comments:"asdlnaskldf"},
+        {username:"user3",dp:logo,comments:"asfklas"},
+        {username:"user4",dp:logo,comments:"asfkaslmfsa"},
+        {username:"user5",dp:logo,comments:"aslfnaskfs"},
+        {username:"user6",dp:logo,comments:"asfkjsnskfjas"},
+        {username:"user7",dp:logo,comments:"aslfnaskfas"},
+        {username:"user8",dp:logo,comments:"asflkaslfkasmf"},
+        {username:"user9s",dp:logo,comments:"saflsakmfs"},
+        
+      ]
+    }
+  ]
   return (
     <div className='main'>
         <Sidebar/>
@@ -18,7 +40,7 @@ const App = () => {
           <Navbar/>
           <div className="main-content">
             <Routes>
-              <Route path='/event' element={<Event/>}/>
+              <Route path='/event' element={<Event loggedIn={loggedIn} usr={usr} setUsr={setUsr} event={event}/>}/>
               <Route path='/' element={<Home/>}/>
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<Signup/>}/>
--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -1,8 +1,108 @@
 import React from 'react'
 import './Event.css'
-const Event = () => {
+import backgroundIMage from '../pics/images.jpg'
+import {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import close from "../pics/close.png"
+import logo from '../pics/x.jpg'
+const Event = ({loggedIn,usr,setUsr,event}) => {
+  const navigate = useNavigate();
+  const [commenting,setCommenting]=useState(false)
+  const [comments,setComments]=useState(event.comments)
+  const addComment=()=>{
+    if(!loggedIn){
+      navigate('/login')
+    }
+    else{
+    console.log(comments)
+    const comment=document.querySelector('.comment-input input')
+    setComments([...comments,{username:usr.username,dp:usr.dp,comments:comment.value}])
+    comment.value=''
+    }
+  }
+  const liking=()=>{
+    if(loggedIn){
+      const isLiked = usr.liked.includes(event.title); 
+      if(isLiked){
+        setUsr((prevUsr) => ({
+          ...prevUsr, 
+          liked: prevUsr.liked.filter((x) => x !== event.title), 
+      }));
+        const button=document.querySelector('.interaction .like')
+        button.innerHTML='Like'
+      }
+      else{
+        setUsr((prevUsr) => ({
+          ...prevUsr, 
+          liked: [...prevUsr.liked, event.title], 
+      }));
+        const button=document.querySelector('.interaction .like')
+        button.innerHTML='Liked'
+        }
+    }
+    else{navigate('/login')}
+    
+  }
+  const registering=()=>{
+    if(loggedIn){
+      const isRegistered = usr.registered.includes(event.title); 
+      if(isRegistered){
+        setUsr((prevUsr) => ({
+          ...prevUsr, 
+          registered: prevUsr.registered.filter((x) => x !== event.title), 
+      }));
+        const button=document.querySelector('.interaction .register')
+        button.innerHTML='Register'
+      }
+      else{
+        setUsr((prevUsr) => ({
+          ...prevUsr, 
+          registered: [...prevUsr.registered, event.title], 
+      }));
+        const button=document.querySelector('.interaction .register')
+        button.innerHTML='Registered'
+        }
+    }
+    else{navigate('/login')}
+  }
   return (
-    <div className="event-main">Event</div>
+    <div className="event-main">
+      <img src={backgroundIMage} className='eventBackgroundImg'  alt="" />
+      <div className="event-content">
+        <h2>{event.title}</h2>
+        <h4>organized by: <span>{event.organiser}</span></h4>
+        <h4>BIO: <span>{event.about}</span></h4>
+        <h4>Location:{event.location}</h4>
+        <h4>Time:{event.time}</h4>
+        <div className="interaction">
+          <button onClick={()=>{setCommenting(true)}}>Comment</button>
+          <button className='like' onClick={()=>{liking()}}>Like</button>
+          <button className='register' onClick={()=>{registering()}}>Register</button>
+        </div>
+
+      </div>
+    {commenting &&
+      <div className="commentBox-background">
+        <div className="commentBox">
+          <h2 style={{fontSize:30+"px"}}>COMMENTS</h2>
+          <div className="content">
+            {comments.map((x)=>{
+              return <div className="comment">
+                <img src={x.dp} className='userDp' alt="" />
+                <p>{x.comments}</p>
+
+              </div>
+            })}
+          </div>
+          <div className="comment-input">
+            <input  type="text" />
+            <button onClick={()=>{addComment()}}>Submit</button>
+          </div>
+        </div>
+        <img src={close} className='close' onClick={()=>{setCommenting(false)}} alt="" />
+      </div>
+    }
+    </div>
   )
 }
 
