638695ad0ab3de32f0d9e7ca543027255f489110
Harry-Joseph9387 <harryjoseph9387@gmail.com>
2025-01-06 13:17:13 +0530

    x

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,11 @@ const EventsInfo=require('./models/EventsInfo')
 const jwt = require('jsonwebtoken');
 const UserActivity=require('./models/UserActivity')
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:[""],
+  methods:["POST","GET","PUT"],
+  credentials:true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
